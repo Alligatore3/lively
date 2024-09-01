a9b951a9ad9f5a3485cfef1f38be17a61b6e2245
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-01 09:07:18 +0200

    Nuxt init

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/mattiazanella/Developments/lively/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      Lively
+    </div>
+  </template>
+  
