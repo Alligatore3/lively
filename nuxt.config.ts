96762c6947d1c84465a750e5103b81f706d0ecac
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-07 12:39:58 +0200

    Add tailwindcss module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr: false
+  ssr: false,
+  modules: [
+    // @see https://tailwindcss.nuxtjs.org/getting-started/installation
+    '@nuxtjs/tailwindcss'
+  ]
 })
