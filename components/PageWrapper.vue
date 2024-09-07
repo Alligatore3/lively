30051954e63d6bc7c109508b14861e2bd02f4134
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-07 13:35:23 +0200

    Add Homepage layout

--- components/PageWrapper.vue
+++ components/PageWrapper.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+/**
+ * '48 => 24 * 2' - '24 => temp MainHeader height' - '1rem => m-1 class below'
+ */
+const height = 'calc(100% - 48px - 1rem)'
+</script>
+
 <template>
-    <div class="w-auto h-auto bg-white m-4 rounded-[20px] overflow-hidden">
+    <div class="w-auto h-auto bg-white m-4 rounded-[20px] overflow-hidden" :style="{ height }">
         <slot />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex">
+  <div class="flex h-full">
     <div class="flex-1 p-4">
       <p>
         Lively - Nuxt
-      </p>o
+      </p>
 
       <NuxtLink to="/agencies">
         But check Agencies
       </NuxtLink>
     </div>
-    <div :style="{ backgroundImage: `url(~@/assets/images/house.jpg)` }" class="flex-1"></div>
+    <div :style="{ backgroundImage: `url(https://images.unsplash.com/photo-1591154669695-5f2a8d20c089` }" class="flex-1 bg-center bg-cover bg-no-repeat"></div>
 </div>
 </template>
   
