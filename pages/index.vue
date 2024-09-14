4ea6dbc9184a7e898958665fe967d1d94ea46ef6
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-14 15:23:50 +0200

    Apply container classes

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,9 @@ const backgroundImage = `url(${houseImage})`;
 <template>
   <div class="w-full h-full">
     <div :style="{ backgroundImage }" class="h-full bg-right-bottom bg-no-repeat">
-      <div class="p-16">
+      <div class="max-w-4xl p-16">
         <h3 class="font-bold text-8xl" v-html="$t('home.title')" />
-        <p class="max-w-[560px] my-4 text-2xl">
+        <p class="my-4 text-2xl">
           {{ $t('home.description') }}
         </p>
       </div>
