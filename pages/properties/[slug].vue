6a6d76e25b7361e50bab8f3ef9783d95906c06c6
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-15 17:49:04 +0200

    Add temp. Masonry

---
+++ pages/properties/[slug].vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+const items = [
+  {
+    src: 'https://plus.unsplash.com/premium_photo-1678903964473-1271ecfb0288?q=80&w=3987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://plus.unsplash.com/premium_photo-1680281937048-735543c5c0f7?q=80&w=2222&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://plus.unsplash.com/premium_photo-1680281937048-735543c5c0f7?q=80&w=2222&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://plus.unsplash.com/premium_photo-1680281937048-735543c5c0f7?q=80&w=2222&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://plus.unsplash.com/premium_photo-1680281937048-735543c5c0f7?q=80&w=2222&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://plus.unsplash.com/premium_photo-1680281937048-735543c5c0f7?q=80&w=2222&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    src: 'https://plus.unsplash.com/premium_photo-1680281937048-735543c5c0f7?q=80&w=2222&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
+
+function chunkArray(items: { src: string }[], chunkSize = 3) {
+  const chunks = [];
+  let i = 0;
+  const n = items.length;
+
+  while (i < n) {
+    const chunk = items.slice(i, i + chunkSize);
+    chunks.push(chunk);
+    i += chunkSize;
+  }
+
+  return chunks;
+}
+
+const chunkedItems = chunkArray(items);
+</script>
+
+<template>
+  <div class="p-4 h-auto max-h-[500px] overflow-auto">
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div :key="index" v-for="(chunk, index) in chunkedItems" class="grid gap-4">
+        <div v-for="image in chunk">
+          <img class="h-auto max-w-full rounded-lg" :src="image.src" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
