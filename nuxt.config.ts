edbbec2362aeacaf1953874c8934a08272ba1df0
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-15 11:22:58 +0200

    Add initPropertiesSearch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  pages: true,
   ssr: false,
   css: ['~/assets/css/main.css'],
   // @see https://tailwindcss.nuxtjs.org/getting-started/installation
   modules: [
     '@nuxtjs/tailwindcss',
     [
-      '@nuxtjs/google-fonts', {
-      families: {
-        Raleway: true,
-      }
-    }],
-    '@nuxtjs/i18n'
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Raleway: true,
+        },
+      },
+    ],
+    '@nuxtjs/i18n',
   ],
   i18n: {
-    vueI18n: './i18n.config.ts' // if you are using custom path, default
-  }
-})
+    vueI18n: './i18n.config.ts', // if you are using custom path, default
+  },
+});
--- pages/properties/index.vue
+++ pages/properties/index.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+const type = ref<'buy' | 'rent' | null>(null);
+
+const location = ref<string | null>(null);
+
+const priceRange = ref<number | null>(null);
+
+/**
+ * @todo 'useRoute' is not updating values.
+ */
+function initPropertiesSearch() {
+  const queryString = window.location.search;
+
+  const urlParams = new URLSearchParams(queryString);
+
+  priceRange.value = Number(urlParams.get('priceRange'));
+  type.value = urlParams.get('type') as 'buy' | 'rent';
+  location.value = urlParams.get('location');
+}
+
+onMounted(initPropertiesSearch);
+</script>
+
 <template>
-  <div>Lively - Properties</div>
+  <div class="p-4">
+    <h1>Lively - Properties</h1>
+
+    Current search:
+    <br />
+    <code>priceRange: {{ priceRange || 'Not defined' }} </code>
+    <br />
+    <code>location: {{ location || 'Not defined' }} </code>
+    <br />
+    <code>type: {{ type }} </code>
+  </div>
 </template>
