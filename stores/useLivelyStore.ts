4fe8a1df81016172a41308144a2325d0b981f89d
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-21 13:21:32 +0200

    Changed contetn-type BE wise

--- stores/useLivelyStore.ts
+++ stores/useLivelyStore.ts
@@ -5,10 +5,8 @@ export function useLivelyStore() {
 
   const token = useState<string | null>('tokenRequest', () => null);
 
-  const convertStringToJSON = (toParse: unknown) => (isString(toParse) ? JSON.parse(toParse) : { token: null });
-
   async function initHelloClient() {
-    const { data, error } = await useFetch(generateLivelyEndpoint('hello/client'));
+    const { data, error } = await useFetch<{ token: string }>(generateLivelyEndpoint('hello/client'));
 
     if (error.value) {
       const text = error.value as unknown as string;
@@ -18,8 +16,7 @@ export function useLivelyStore() {
         text,
       });
     } else {
-      const { token: parsedToken } = convertStringToJSON(data.value);
-      token.value = parsedToken;
+      token.value = data.value?.token || null;
     }
   }
 
