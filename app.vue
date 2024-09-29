d212843c28de59fed63b72af80938e790e05bee0
Mattia Zanella <mattia_zanella@icloud.com>
2024-09-29 19:27:47 +0200

    Improve SEO iwth useHead

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+useHead({
+  title: 'Lively - Real Estate Platform',
+  link: [{ href: 'favicon.png', type: 'image/png', rel: 'icon' }],
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Lively can help you rent, sell, or buy accomodation, take a mortage, invest in real estate, and a lot more',
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
 
